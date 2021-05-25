b974b470b8a5ff250579a076326296d9d901a969
KSpringborn <kevin.springborn@gmail.com>
2021-05-25 23:12:45 +0200

    Revert "Test Maps Branch"
    This reverts commit 6cdff602

--- strollr/ios/Runner/AppDelegate.swift
+++ strollr/ios/Runner/AppDelegate.swift
@@ -1,6 +1,5 @@
 import UIKit
 import Flutter
-import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,8 +7,6 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    //Add your Google Maps API Key here
-    GMSServices.provideAPIKey("AIzaSyD_tu2xAcc4YsaGGSDG2OQNfNFLXRHAiFA")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
