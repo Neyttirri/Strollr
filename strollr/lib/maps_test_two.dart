389938daaaa99c515cba4ef0552bffa10846cff3
KSpringborn <kevin.springborn@gmail.com>
2021-06-03 05:35:13 +0200

    version 2 after mistake deletion.. still not fixed to old version

---
+++ strollr/lib/maps_test_two.dart
@@ -0,0 +1,73 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Google Maps Demo',
+      home: MapSample(),
+    );
+  }
+}
+
+class MapSample extends StatefulWidget {
+  @override
+  State<MapSample> createState() => MapSampleState();
+}
+
+class MapSampleState extends State<MapSample> {
+  GoogleMapController _controller;
+
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  static final CameraPosition _kLake = CameraPosition(
+      bearing: 192.8334901395799,
+      target: LatLng(37.43296265331129, -122.08832357078792),
+      tilt: 59.440717697143555,
+      zoom: 19.151926040649414);
+
+  // void changeMapStyle() {                                                  //add String path as parameter
+  //     getJsonMapStyle("assets/mapsBundle/mapsDefaultView.json").then(setMapStyle);  //exchange "" with String path parameter just for testing
+  // }
+  //
+  // Future<String> getJsonMapStyle(String path) async {
+  //   return await rootBundle.loadString(path);
+  // }
+  //
+  // void setMapStyle(String mapStyle) {
+  //   _controller.setMapStyle(mapStyle);
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    // changeMapStyle();
+    return new Scaffold(
+      body: GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: _kGooglePlex,
+        onMapCreated: (GoogleMapController controller) {
+          // changeMapStyle();
+        },
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: _goToTheLake,
+        label: Text('To the lake!'),
+        icon: Icon(Icons.directions_boat),
+      ),
+    );
+  }
+
+  Future<void> _goToTheLake() async {
+    GoogleMapController _controller;
+    _controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+  }
+}
