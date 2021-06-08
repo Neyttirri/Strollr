f436da4a07fc4b1266333e10d9f06aab624e4186
CoalaCode <max.schneider@student.htw-berlin.de>
2021-06-08 14:08:39 +0200

    Add Collection Category Card

---
+++ strollr/lib/collection_pages/collection_category_card.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CollectionListCard {
+  String categoryImage;
+  String categoryName;
+  int itemCount;
+
+
+  CollectionListCard(this.categoryImage, this.categoryName, this.itemCount);
+}
