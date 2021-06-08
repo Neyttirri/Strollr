b39b21578a94c6d69262db9e88424556131e4eec
CoalaCode <max.schneider@student.htw-berlin.de>
2021-06-08 14:09:31 +0200

    Added Collection Gallery View

---
+++ strollr/lib/collection_pages/collection_gallery_view.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:strollr/collection_pages/steckbrief.dart';
+
+import '../style.dart';
+
+class GalleryView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Gallery View", style: TextStyle(color: headerGreen)),
+        backgroundColor: Colors.white,
+      ),
+      body: mainWidget(),
+    );
+  }
+
+  Widget mainWidget() {
+    return gridBuild();
+  }
+
+  Widget gridView() {
+    return GridView.extent(
+      maxCrossAxisExtent: 150,
+      padding: const EdgeInsets.all(4),
+      mainAxisSpacing: 4,
+      crossAxisSpacing: 4,
+      children: _buildGridList(25),
+    );
+  }
+
+  Widget gridBuild() {
+    return GridView.builder(
+      gridDelegate: new SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 4),
+      itemCount: 45,
+      itemBuilder: (BuildContext context, int index) {
+        return new GestureDetector(
+          onTap: () {
+            Navigator.of(context).push(MaterialPageRoute(builder: (context) => Steckbrief()));
+          },
+          child: Container(
+            child: Image.asset("assets/images/treeIcon.png"),
+          ),
+        );
+      },
+    );
+  }
+}
+
+List<Container> _buildGridList(int i) => List.generate(
+    i, (index) => Container(
+  child: Image.asset("assets/images/mushroomIcon.png"),
+)
+);
