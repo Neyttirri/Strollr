333b68f0ba1c39f51b5611c51642300f1d273b9c
Antje Stockhaus <s0573662@htw-berlin.de>
2021-06-27 11:50:31 +0100

    created save_route UI

--- strollr/lib/route_pages/save_route.dart
+++ strollr/lib/route_pages/save_route.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:strollr/Tabs/routes.dart';
 import '../style.dart';
 
+final _formKey = GlobalKey<FormState>();
+
 class RouteSaver extends StatefulWidget {
   _RouteSaverState createState() => _RouteSaverState();
 }
@@ -17,11 +19,12 @@ class _RouteSaverState extends State<RouteSaver> {
         ),
         body: Center(
             child: Container(
-          padding: EdgeInsets.fromLTRB(10, 20, 10, 20),
-          child: Column(
-            children: [RouteForm()],
-          ),
-        )));
+                padding: EdgeInsets.fromLTRB(10, 20, 10, 20),
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [RouteForm(), buttonRow(context)],
+                  ),
+                ))));
   }
 }
 
@@ -33,7 +36,7 @@ class RouteForm extends StatefulWidget {
 }
 
 class RouteFormState extends State<RouteForm> {
-  final _formKey = GlobalKey<FormState>(); //related to WalkID
+  //related to WalkID
 
   @override
   Widget build(BuildContext context) {
@@ -53,25 +56,83 @@ class RouteFormState extends State<RouteForm> {
               return null;
             },
           ),
+          SizedBox(
+            height: 250,
+          ), //insert map
           Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16.0),
-            child: ElevatedButton(
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(Colors.green),
-              ),
-              onPressed: () {
-                if (_formKey.currentState!.validate()) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(content: Text('Route wird gespeichert')));
-                  Navigator.of(context)
-                      .push(MaterialPageRoute(builder: (context) => Routes()));
-                }
-              },
-              child: Text('speichern'),
+            padding: EdgeInsets.fromLTRB(0, 10, 10, 10),
+            child: Row(
+              children: <Widget>[
+                Text("Datum:"),
+                Spacer(),
+                Text("2020"),
+              ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(0, 10, 10, 10),
+            child: Row(
+              children: <Widget>[
+                Text("Strecke in km:"),
+                Spacer(),
+                Text("12,42 km"),
+              ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(0, 10, 10, 10),
+            child: Row(
+              children: <Widget>[
+                Text("Zeit in Stunden:"),
+                Spacer(),
+                Text("4:32 h"),
+              ],
             ),
           ),
         ],
       ),
     );
   }
+}
+
+Widget saveButton(BuildContext context) {
+  return ElevatedButton(
+    style: ButtonStyle(
+      backgroundColor: MaterialStateProperty.all(Colors.green),
+    ),
+    onPressed: () {
+      if (_formKey.currentState!.validate()) {
+        ScaffoldMessenger.of(context)
+            .showSnackBar(SnackBar(content: Text('Route wird gespeichert')));
+        Navigator.of(context)
+            .push(MaterialPageRoute(builder: (context) => Routes()));
+      }
+    },
+    child: Text('speichern'),
+  );
+}
+
+Widget deleteButton(BuildContext context) {
+  return ElevatedButton(
+    style: ButtonStyle(
+      backgroundColor: MaterialStateProperty.all(Colors.grey),
+    ),
+    onPressed: () {
+      if (_formKey.currentState!.validate()) {
+        ScaffoldMessenger.of(context)
+            .showSnackBar(SnackBar(content: Text('Route wird gelöscht')));
+        Navigator.of(context)
+            .push(MaterialPageRoute(builder: (context) => Routes()));
+      }
+    },
+    child: Text(' Route löschen'),
+  );
+}
+
+Widget buttonRow(BuildContext context) {
+  return Container(
+      child: Row(
+    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    children: [deleteButton(context), saveButton(context)],
+  ));
 }
