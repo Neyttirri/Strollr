8a19070e8e94b0177fc39fe477a9ea1afac0e11e
CoalaCode <max.schneider@student.htw-berlin.de>
2021-07-11 21:53:15 +0200

    Splash Screen

--- strollr/lib/maps_test_two.dart
+++ strollr/lib/maps_test_two.dart
@@ -291,11 +291,11 @@ class _MapViewState extends State<MapView> {
 
   void loadMarkerIcons() {
     var resources = [
-      "assets/images/defaultIcon_marker.png",
-      "assets/images/animalFootstepIcon_marker.png",
-      "assets/images/treeIcon_marker.png",
-      "assets/images/plantIcon_marker.png",
-      "assets/images/mushroomIcon_marker.png"
+      "assets/images/defaultMapIcon.png",
+      "assets/images/animalFootstepMapIcon.png",
+      "assets/images/treeMapIcon.png",
+      "assets/images/plantMapIcon.png",
+      "assets/images/mushroomIMapIcon.png"
     ];
     for (String res in resources) {
       BitmapDescriptor.fromAssetImage(
